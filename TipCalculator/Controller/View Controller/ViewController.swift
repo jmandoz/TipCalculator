00fc7fd3eed724a0810b1e28e51e5c646c4d0047
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-07-15 15:51:18 -0600

    Set up the Views, model, and functions

---
+++ TipCalculator/Controller/View Controller/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Jason Mandozzi on 7/15/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var calculatedTipLabel: UILabel!
+    
+    @IBOutlet weak var numberOfPeopleTextField: UITextField!
+    
+    @IBOutlet weak var pricePerPersonLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func tenPercentButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func fifteenPercentButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func twentyPercentButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func twentyFivePercentButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func calculateButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
+
---
+++ TipCalculator/Model/TipCalculator.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculator.swift
+//  TipCalculator
+//
+//  Created by Jason Mandozzi on 7/15/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import Foundation
+
+struct TipCalculator {
+    static var bill: Double = 0
+    static var billWithTip: Double = 0
+    static var numberOfPeople: Int = 0
+    static var pricePerPerson: Double = 0
+    }
+
